4149abc262657d14e837d13bad600e64c274ca7e
ahmedBoukhtioua <ahmed.boukhtioua@esprit.tn>
2020-06-13 19:25:19 +0200

    front

---
+++ RCV/RCV-FRONT/rcv-front/src/app/component/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from "../../services/authentication.service";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  form: any = {};
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  constructor(private authenticatiobService: AuthenticationService) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    this.authenticatiobService.register(this.form).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
+}
